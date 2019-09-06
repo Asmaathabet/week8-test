ce1d02e9e175b3180e6797844a32dfad1a74b81d
Asmaathabet <asmathabet2009@hotmail.com>
2019-09-06 11:26:41 +0300

    addUser query

--- server/database/queries/addUser.js
+++ server/database/queries/addUser.js
@@ -1,1 +1,10 @@
 // Write a query to add the user and their password to the database
+const dbConnection = require('../config/connection');
+
+exports.addUser = data => {
+    const { email, hashedPW } = data;
+    return dbConnection.query({
+        text: 'INSERT INTO users (email , password) VALUES ($1,$2) RETURNING *; ',
+        values: [email, hashedPW],
+    });
+};
