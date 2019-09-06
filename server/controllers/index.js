324111b4eb338a06eed1a98b010f38ca4684ad44
Asmaathabet <asmathabet2009@hotmail.com>
2019-09-06 21:13:25 +0300

    make Logout middleware

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,13 +4,18 @@ const error = require('./error');
 const city = require('./city');
 const signup = require('./signup');
 const login = require('./login');
+const logout = require('./logout');
 
 const router = express.Router();
 
 router.get('/login', login.renderLogin);
 router.post('/login', login.postLogin);
+
 router.get('/signup', signup.renderSignup);
 router.post('/signup', signup.postSignup);
+
+router.get('/logout', logout.logout);
+
 router.get('/cities', city.renderCities);
 router.get('/all-cities', city.getAllCities);
 router.post('/add-city', city.add);
